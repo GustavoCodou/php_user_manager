b913decdf0ae5ed2282a5d4571fd067b0e98a9ec
Gustavo Guimarães de Sousa <167660193+GustavoCodou@users.noreply.github.com>
2025-08-19 20:51:09 -0300

    .

--- partials/header.php
+++ partials/header.php
@@ -1,22 +1,9 @@
-<?php
+<link rel="stylesheet" href="css/header.css">
 
-session_start();
-include 'db.php';
-?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login App with SQL and PHP</title>
-
-    <style>
-
-
-
-    </style>
-
-</head>
-<body>
+<header>
+  <a href="index.php">Home</a>
+  <a href="admin.php">Admin</a>
+  <a href="register.php">Register</a>
+  <a href="login.php">Login</a>
+  <a href="logout.php">Logout</a>
+</header>
--- partials/navigation.php
+++ partials/navigation.php
@@ -1,34 +1,45 @@
-<nav>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/navigation.css"> <!-- aqui conecta o CSS -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+    <nav>
+
+        <ul>
+
+    <!-- Aqui dependendo se estiver logado aparece uma opção diferente-->
+    <?php if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true): ?>
+
+            <a href="admin.php">Admin</a>
 
 
 
-    <ul>
+            <a href="logout.php">Logout</a>
 
+
+    <?php else: ?>
+
+
+            <a href="register.php">Register</a>
+
+
+
+            <a href="login.php">login</a>
+
+
+    <?php endif; ?>
+
+        </ul>
+
+    </nav>
     
+</body>
+</html>
 
 
-<!-- Aqui dependendo se estiver logado aparece uma opção diferente-->
-<?php if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true): ?>
-    <li>
-        <a href="admin.php">Admin</a>
-    </li>
 
-    <li>
-        <a href="logout.php">Logout</a>
-    </li>
 
-<?php else: ?>
-
-    <li>
-        <a href="register.php">Register</a>
-    </li>
-
-    <li>
-        <a href="login.php">login</a>
-    </li>
-
-<?php endif; ?>
-
-    </ul>
-
-</nav>
